576f06577402d0f832a20776df94c9865ebe88e6
no-one <none>
2022-11-22 08:40:48 +0545

    2022-11-9

--- aman/Form1.cs
+++ aman/Form1.cs
@@ -3,8 +3,13 @@ namespace aman
     public partial class Form1 : Form
     {
         public Form1()
-        {
+      a  {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
